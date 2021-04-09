31faa0e5ae78366f3fc09e1cce726d376b5c594d
Vladimir <novel@MSI.localdomain>
2021-04-09 10:59:41 +0700

    Add the adjacency matrix & the main functions to work with it (create, init, print). Fix Deep First Searching function.

--- LAB_03_GRAPH/graph.cpp
+++ LAB_03_GRAPH/graph.cpp
@@ -2,21 +2,29 @@
 
 graph *graph_create(int vertexes);
 void graph_init(graph *g);
+int **matrix_create(graph *g);
+void matrix_init(int **mx, int size);
+void matrix_print(int **matrix, int size_m);
 
 int main () {
 
     graph *g;
-    g = graph_create(10);
+    g = graph_create(5);
+
+    int **matrix = (int**)malloc(g->vertexes*sizeof(int*));
 
     graph_set_edge_weight(g, 1, 2, 1);
     graph_set_edge_weight(g, 1, 4, 1);
-    graph_set_edge_weight(g, 5, 4, 1);
-    graph_set_edge_weight(g, 6, 4, 1);
+    graph_set_edge_weight(g, 2, 4, 1);
+    graph_set_edge_weight(g, 2, 3, 1);
     graph_set_edge_weight(g, 3, 4, 1);
 
     cout << "DFS : \n";
     graph_DFS(g, 2);
     graph_print(g);
+
+    matrix = (matrix_create(g));
+    matrix_print(matrix, g->vertexes);
 
     graph_free(g);
 
@@ -78,23 +86,57 @@ int graph_get_edge_weight(graph *g, int i, int j) {
 void graph_DFS(graph *g, int v) {
     int i;
 
-    g->visited[v -1] = 1;
+    g->visited[v - 1] = 1;
     cout << "Vertex : " << v << "\n";
 
     for (i = 0; i < g->vertexes; i++) 
     {
-        if (g->m[v][i] > 0 && g->visited[i] == 0)
+        if (g->m[v - 1][i] > 0 && g->visited[i] == 0)
             graph_DFS(g, i + 1);
     }
 }
 
 void graph_print(graph *g) {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < g->vertexes; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < g->vertexes; j++)
         {
             cout << g->m[i][j] << " ";
-            if (j == 9)
+            if (j == g->vertexes - 1)
+                cout << "\n";
+        }
+    }
+}
+
+int **matrix_create(graph *g) {       //n - размер графа
+
+    int m_size = (g->vertexes)*(g->vertexes);
+    int **mx;
+
+    mx = (int**)malloc(m_size*sizeof(int*));
+
+    for (int i = 0; i < m_size; i++) { 
+        mx[i] = (int*)malloc(m_size*sizeof(int)); 
+    }
+
+    matrix_init(mx, m_size);
+
+    return mx;
+}
+
+void matrix_init(int **mx, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            mx[i][j] = 0;
+        }
+    }
+}
+
+void matrix_print(int **matrix, int size_m) {
+    for (int i = 0; i < (size_m)*(size_m); i++) {
+        for (int j = 0; j < (size_m)*(size_m); j++) {
+            cout << matrix[i][j] << " ";
+            if (j == (size_m)*(size_m) - 1)
                 cout << "\n";
         }
     }
